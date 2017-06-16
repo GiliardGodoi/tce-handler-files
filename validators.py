4334f454388e748683c34dd2731adb6aa56e2684
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-06-16 18:40:04 -0300

    alterações nos métodos valide, is_decimal e _convert_to_decimal. renomeando método _strip. agrecação do objeto DB. Sabe onde salvar um objeto.

--- validators.py
+++ validators.py
@@ -1,18 +1,31 @@
 import re
+# from db import MyMongoDBInstance as DB
+from db import MyFileSaveOption as DB
 
 class Validator():
     def __init__(self):
+        self.db = DB()
+        self.collection_name = 'teste'
         self.intFields = []
         self.floatFields = []
         self.schema = []
     
     def valide(self, registro):
-        return registro
-    
+        novo = {}
+        for chave in list(registro.keys()):
+            if type(registro[chave]) is str :
+                novo[chave] = registro[chave].strip()
+            if chave in self.floatFields :
+                novo[chave] = self._convert_to_decimal(registro[chave])
+        return novo
+
+    def save(self,data):
+        self.db.save(data,self.collection_name)
+
     def is_decimal(self,string): # testa por expressao regular se a string possui o foramto 0000.0000
         match_object = re.fullmatch(r'\d+\.?\d*',string)
         if match_object :
-            result = True if match_object.group() == string else False
+            result = True if match_object.group() else False
             return result
         else :
             return False
@@ -31,13 +44,15 @@ class Validator():
         except ValueError:
             raise ValueError("Nao foi possivel converter o valor para int: "+ value)
 
-
-    def _strip_field(self,registro,chave):
+    def _strip(self,novo_obj, registro,chave):
         if type(registro[chave]) is str :
-            registro[chave] = registro[chave].strip()
-        return registro
+            novo_obj[chave] = registro[chave].strip()
+        return novo_obj
     
-    def _convert_to_decimal(self, registro, chave):
-        if self.is_decimal(registro[chave]) :
-            registro[chave] = self._to_decimal(registro[chave])
+    def _convert_to_decimal(self, value):
+        # if self.is_decimal(value) :
+        #     return self._to_decimal(value)
+        # else:
+        #     return value
+        return self._to_decimal(value)
 
