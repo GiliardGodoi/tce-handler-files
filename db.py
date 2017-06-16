2152341febbaa0f65e6bda445aae928f12ffa056
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-06-16 17:22:28 -0300

    Adicionando classe para salvar dados em arquivo .json

--- db.py
+++ db.py
@@ -20,3 +20,19 @@ class MyMongoDBInstance():
             print('criando colecao: ',collection)
             db.collection.insert_many(data, ordered=False)
         # return db.insert_many(data,ordered=False)
+    
+class MyFileSaveOption():
+    def __init__(self):
+        print('To save at files')
+        import codecs
+        import json
+        import os
+
+    def save(self,data,file_name):
+        with codecs.open(file_name, encoding='utf-8', mode='a') as file:
+            for d in data:
+                file.write(self.dict_to_json(d))
+            return True
+    
+    def dict_to_json(self,dict_data):
+        return json.JSONEncoder(ensure_ascii=False,indent=2).encode(dict_data)
