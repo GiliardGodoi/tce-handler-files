7310204329463d4bfb79f30d40bad5f4bd727c1c
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-06-27 02:53:30 -0300

    Evitando exceções por tentar inserir um documento vazio. Alterando o nome do banco padrão para se conectar. Determinar se um arquivo possui cabeçalho.

--- parser/db.py
+++ parser/db.py
@@ -5,7 +5,7 @@ import os
 
 class MongoStorage():
 
-    def __init__(self, db_name = 'public_data'):
+    def __init__(self, db_name = 'raw_data'):
         self.db_name = db_name
         self.collections = []
     
@@ -16,20 +16,22 @@ class MongoStorage():
         self.collections = db.collection_names()
         return db
 
-    def save(self, data, collection = None):
+    def save(self, data = [], collection = None):
+        if not data :
+            return 0
         db = self.get_db()
         result = {}
         insert_result = None
         if collection in self.collections :
-            result = db[collection].insert_many(data, ordered=False)
+            insert_result = db[collection].insert_many(data, ordered=False)
         else :
             print('criando colecao: ',collection)
             insert_result = db[collection].insert_many(data, ordered=False)
         # return db.insert_many(data,ordered=False)
         if insert_result :
-            result['inserted_lenght'] = len(insert_result.inserted_ids) #TypeError: 'InsertManyResult' object does not support item assignment
-            result['inserted_ids'] = insert_result.inserted_ids
-            return result
+            # result['inserted_lenght'] = len(insert_result.inserted_ids) #TypeError: 'InsertManyResult' object does not support item assignment
+            # result['inserted_ids'] = insert_result.inserted_ids
+            return len(insert_result.inserted_ids)
         else :
             return None
     
@@ -37,26 +39,47 @@ class FileJSONStorage():
             
     def save(self,data,file_name):
         file_name += '.json'
+        qtd = len(data)
         with codecs.open(file_name, encoding='utf-8', mode='a') as file:
             for d in data:
                 file.write(self.dict_to_json(d))
-            return True
+        return qtd
     
     def dict_to_json(self,dict_data):
         return json.JSONEncoder(ensure_ascii=False,indent=2).encode(dict_data)
 
 class FileCSVStorage():
+    def __init__(self):
+        csv.register_dialect('mydialect', delimiter=',',lineterminator='\n' )
+        self.sniffer = csv.Sniffer()
+
     def save(self, data, file_name):
         file_name += '.csv'
         field_names = self.define_header(data)
-        with open(file_name,'w') as csvfile:
-            try:
-                writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        nro = len(data)
+        csvfile =  open(file_name,'a+')
+        try:
+            writer = csv.DictWriter(csvfile, fieldnames=field_names,dialect='mydialect')
+            testHeader = self.has_header(csvfile)
+            if testHeader : 
+                print('printando header')
                 writer.writeheader()
-                writer.writerows(data)
-            except Exception as error:
-                raise error
+            writer.writerows(data)
+        except Exception as error:
+            raise error
+        finally :
+            csvfile.close()
+        return nro
     
+    def has_header(self, csvfile):
+        csvfile.seek(0)
+        texto = csvfile.read( (1024 * 2) )
+        csvfile.seek(0)
+        if csv.Sniffer().has_header(texto) :
+            return True
+        else:
+            return False
+
     def define_header(self, data):
         conjunto = set()
         if type(data) is list:
