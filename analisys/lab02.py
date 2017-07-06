b971f4c633c740eeed5ed90928524bd76408f8fc
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-07-06 14:48:04 -0300

    agrupando participantes

--- analisys/lab02.py
+++ analisys/lab02.py
@@ -1,9 +1,17 @@
+'''
+    
+'''
 from pymongo import MongoClient
 
 class ProcessData():
+    '''
+        ROTINA PREPARAÇÃO DOS DADOS
+        rota
+            modalidadePorTipoAvaliacao/:cdIBGE&:nrAno
+    '''
     def __init__(self):
         self._db = None
-
+    
     def get_db(self):
         if not self._db:
             client = MongoClient("mongodb://localhost:27017")
@@ -11,16 +19,41 @@ class ProcessData():
         return self._db
     
     def process(self):
+        db = self.get_db()
+        lstCodIBGE = db.rawLicitacao.distinct('cdIBGE')
+        print('Executando para:\n',lstCodIBGE)
+        for cod in lstCodIBGE:
+            cursor = self.agrupar_fornecedores_por_idlicitacao(cod)
+            count = self.adicionar_participante(cursor)
+            print('cdIBGE: ', cod,' documentos atualizados: ', count)
 
-    def agrupar_fornecedor(self,cdIBGE):
-        match = { "$match" : {"sgDocParticipanteLicitacao" : "CNPJ"} }
-        lookup = { "$lookup" : {
-            "from" : "rawLicitanteVencedor",
-            "localField" : "idLicitacao",
-            "foreignField" : "idlicitacao",
-            "as" : "item"
+    def agrupar_fornecedores_por_idlicitacao(self, cdIBGE):
+        match = {"$match" : {"cdIBGE" : cdIBGE }}
+        group = { "$group" : {
+            "_id" : "$idLicitacao",
+            "nrTotalParticipante" : {"$sum" : 1},
+            "lsParticipante" : { "$push" : {
+                "nmFornecedor" : "$nmParticipanteLicitacao",
+                "sgDocumentoForncedor" : "$sgDocParticipanteLicitacao",
+                "nrDocumentoFornecedor" : "$nrDocParticipanteLicitacao" 
+            } } 
         }}
-        
+        db = self.get_db()
+        cursor = db.rawLicitacaoParticipante.aggregate([match, group])
+        return cursor
+
+    def adicionar_participante(self, cursor = None):
+        if not cursor:
+            raise AttributeError('cursor not valid')
+        db = self.get_db()
+        count = 0
+        for doc in cursor:
+            atualizar = {"participante" : {"nrTotalParticipante" : doc["nrTotalParticipante"], "lsParticipante" : doc['lsParticipante']}}
+            resultado = db.rawLicitacao.update_one({'idLicitacao' : doc['_id']}, {"$set" : atualizar })
+            if resultado:
+                count +=1 
+        return count
+
 
 if __name__ == "__main__":
     p = ProcessData()
