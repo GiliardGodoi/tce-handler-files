4b332308d465c4478547cbe8a1a05d9b26fa4ad1
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-07-11 17:26:55 -0300

    ranking para fornecedores

--- analisys/lab05.py
+++ analisys/lab05.py
@@ -1,37 +1,108 @@
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
+import time
 class ProcessData():
+    '''
+        Rotina para preparação dos dados
+        CAMINHO ROTA:
+        rankingFornecedor/
+        
+        MÉTODO: GET
+        
+        PARÂMETROS:
+        ?cdIBGE=[cdIBGE]&nrAno=[nrAno]&typeRanking=[tipoRanking]&limit=[nrlimit]&sort=[asc || desc]
+        tipoRanking = valor || presenca
+
+        OBJETIVO
+        Para determinado ano e Municipio, determinar lista  das empresas:
+        com maiores valores contratados
+        
+        MODELO DE DADO
+
+        cdIBGE : <string>,
+        nmMunicipio : <string>,
+        fornecedor : {
+            nmFornecedor : <string>,
+            nrDocumento : <string>,
+            vlContratado : <double>
+        }
+    '''
     def __init__(self):
         self._db = None
+        self._db_name = 'raw_data'
+        self.output_collection = 'rankingFornecedor'
+
+
     def get_db(self):
+        ''' me '''
         if not self._db:
             client = MongoClient("mongodb://localhost:27017")
-            self._db = client['raw_data']
+            self._db = client[self._db_name]
         return self._db
-
-    def process(self):
-        db = self.get_db()
-        lsCodEntidade = db.rawLicitacao.distinct('cdEntidade')
-        for cod in lsCodEntidade:
-            cursor = self.agrupar_ano_modalidade_avaliacao(cod)
     
-    def agrupar_ano_modalidade_avaliacao(self,cod):
-        match = {"$match" : {"cdEntidade" : cod}}
-        group = { "$group" :
-            "_id" :{
-                "nrAno" : "$nrAnoLicitacao",
-                "dsModalidade" : "$dsModalidadeLicitacao"
-                "dsAvaliacao" : "$dsAvaliacaoLicitacao"
-            },
-            "total" : {'$sum' : 1 },
-            "vlLicitado" : { '$sum' : "$vlLicitado"},
-            'vlAdquirido' : {'$sum' : "$vlTotalAdquiridoLicitacao"}
-        }
-        pipeline = [match, group]
+    def execute(self):
+        anos = ['2013', '2014', '2015', '2016']
+        municipios = self.determinar_municipios_para_execucao()
+        for cdIBGE in municipios:
+            for ano in anos:
+                pipe = self.determinar_pipeline(cdIBGE=cdIBGE,nrAno=ano)
+                cursor = self.executar_consulta(pipe)
+                self.salvar_resultado(cursor=cursor,cdIBGE=cdIBGE,nrAno=ano)
+
+    def determinar_municipios_para_execucao(self):
+        ''' retorna uma lista dos municipios para processamento
+        da para melhorar
+        '''
         db = self.get_db()
-        cursor = db.rawLicitacao.aggregate(pipeline)
+        return db.rawLicitacaoVencedor.distinct('cdIBGE')
+    
+    def determinar_pipeline(self,cdIBGE = None, nrAno = None, typeRanking = None, limit = None, sortType = None ):
+        if not cdIBGE:
+            raise AttributeError('cdIBGE deve ser informado')
+        if not nrAno:
+            raise AttributeError('nrAno deve ser informado')
+        # if not typeRanking in ['valor', 'presenca'] :
+        #     raise AttributeError('typeRanking deve ser valor ou presenca')
+        
+        match = {"$match" : {"cdIBGE" : cdIBGE, "nrAnoLicitacao" : nrAno} }
+        group = { "$group" : {
+            "_id" : {"nrDocumento" : "$nrDocumento","nmFornecedor" : "$nmFornecedor"},
+            "vlContratado" : { "$sum" : "$vlTotalVencedorLicitacao" }
+        }}
+        sort = { "$sort" : {"vlContratado" : -1 }}
+
+        pipeline = [match, group, sort]
+        return pipeline
+
+
+    def executar_consulta(self, pipeline):
+        db = self.get_db()
+        cursor = db['rawLicitacaoVencedor'].aggregate(pipeline)
         return cursor
+    
+    def salvar_resultado(self, cursor, output_collection = None, cdIBGE = None, nrAno = None):
+        if not output_collection:
+            output_collection = self.output_collection
+        if not (cdIBGE and nrAno):
+            raise AttributeError('aux deve fornecer informacoes sobre a cdIBGE e ano')
+        db = self.get_db()
+        for doc in cursor:
+            obj = {}
+            obj['_id'] = ObjectId()
+            obj['cdIBGE'] = cdIBGE
+            obj['nrAno'] = nrAno
+            obj['fornecedor'] = {}
+            obj['fornecedor']['nmFornecedor'] = doc['_id']['nmFornecedor']
+            obj['fornecedor']['nrDocumento'] = doc['_id']['nrDocumento']
+            obj['fornecedor']['vlContratado'] = doc['vlContratado']
+            db[output_collection].insert_one(obj)
+
 
 if __name__ == "__main__":
-    p = ProcessData()
-    p.process() 
+    P = ProcessData()
+    print('Iniciando execucao: ')
+    INI = time.time()
+    P.execute()
+    FIM = time.time()
+    print('tempo de execução: ', str(FIM-INI))
